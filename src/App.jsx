ace4ad094c945226c4f82cf4876701f4239c4b31
vladian95 <kvankvan1995@gmail.com>
2023-03-02 13:07:22 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import Contact from './components/Contact';
+import Hero from './components/Hero';
+import Who from './components/Who';
+import Works from './components/Works';
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: rebeccapurple;
+`;
+
+function App() {
+  return (
+    <Container>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </Container>
+  );
+}
+
+export default App;
