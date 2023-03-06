888e1bf02864063155ddc0d2f2bf5b2e33a05650
vladian95 <kvankvan1995@gmail.com>
2023-03-06 13:35:37 +0300

    hero and who

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -4,10 +4,86 @@ import styled from 'styled-components';
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+`;
+
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 5px;
+`;
+
+const Subtitle = styled.h1`
+  color: #da4ea2;
+`;
+
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgray;
+`;
+
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  width: 120px;
+  padding: 10px;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
 `;
 
 const Who = () => {
-  return <Section></Section>;
+  return (
+    <Section>
+      <Container>
+        <Left></Left>
+        <Right>
+          <Title>Think outside the square space</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>What we are</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit
+            reiciendis in temporibus minus! Dolor veritatis alias non dolorem
+            praesentium sint dolores aliquid quisquam! Distinctio quasi
+            dignissimos eius illum aliquam beatae?
+          </Desc>
+          <Button>See our works</Button>
+        </Right>
+      </Container>
+    </Section>
+  );
 };
 
 export default Who;
