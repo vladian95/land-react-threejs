5712c4443825267019d1ffe06093732c1735d3fb
vladian95 <kvankvan1995@gmail.com>
2023-03-09 11:35:23 +0300

    maps done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Map from './Map';
 
 const Section = styled.div`
   height: 100vh;
@@ -73,7 +74,9 @@ const Contact = () => {
             <Button>Send</Button>
           </Form>
         </Left>
-        <Right></Right>
+        <Right>
+          <Map />
+        </Right>
       </Container>
     </Section>
   );
---
+++ src/components/Map.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Annotation,
+  ZoomableGroup,
+} from 'react-simple-maps';
+
+const Map = () => {
+  return (
+    <ComposableMap
+      projection="geoAzimuthalEqualArea"
+      projectionConfig={{
+        rotate: [-10.0, -52.0, 0],
+        center: [-5, -3],
+        scale: 250,
+      }}
+      style={{ width: '100%', height: '100%' }}
+    >
+      <Geographies
+        geography="/features.json"
+        fill="#2c065d"
+        stroke="#FFFFFF"
+        strokeWidth={0.5}
+      >
+        {({ geographies }) =>
+          geographies.map((geo) => (
+            <Geography key={geo.rsmKey} geography={geo} />
+          ))
+        }
+      </Geographies>
+      <Annotation
+        subject={[]}
+        dx={-60}
+        dy={-30}
+        connectorProps={{
+          stroke: 'transparent',
+          strokeWidth: 1,
+          strokeLinecap: 'round',
+        }}
+      >
+        <text
+          x="-8"
+          textAnchor="end"
+          alignmentBaseline="middle"
+          fill="transparent"
+        >
+          {''}
+        </text>
+      </Annotation>
+    </ComposableMap>
+  );
+};
+
+export default Map;
