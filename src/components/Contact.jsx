406b245cd19483cc17f53580df3411267c7160b9
vladian95 <kvankvan1995@gmail.com>
2023-03-09 11:12:05 +0300

    start maps

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,8 +6,77 @@ const Section = styled.div`
   scroll-snap-align: center;
 `;
 
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const Form = styled.form`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Input = styled.input`
+  padding: 20px;
+  background-color: #e8e6e6;
+  border: none;
+  border-radius: 10px;
+`;
+
+const TextArea = styled.textarea`
+  padding: 20px;
+  background-color: #e8e6e6;
+  border: none;
+  border-radius: 10px;
+`;
+
+const Button = styled.button`
+  background-color: #da4ea2;
+  border: none;
+  border-radius: 10px;
+  font-weight: bold;
+  cursor: pointer;
+  padding: 20px 0px;
+  color: white;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+
 const Contact = () => {
-  return <Section></Section>;
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <Form>
+            <Title>Contact Us</Title>
+            <Input placeholder="Name" />
+            <Input placeholder="Email" />
+            <TextArea placeholder="Write you message" rows={10} />
+            <Button>Send</Button>
+          </Form>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
+  );
 };
 
 export default Contact;
